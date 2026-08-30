15dd87959e421a3e7c94b74a1f38b8b22256533e
Joshua Pacho <102531192+joshdevkit@users.noreply.github.com>
2026-08-30 20:02:58 +0800

    Make employee table mobile responsive

--- resources/js/components/employees/EmployeeTable.tsx
+++ resources/js/components/employees/EmployeeTable.tsx
@@ -3,31 +3,48 @@ import { EmployeeTableRow } from './EmployeeTableRow';
 
 type EmployeeTableProps = {
     employees: Employee[];
-    onMenuToggle: (employee: Employee, event: React.MouseEvent<HTMLButtonElement>) => void;
+    onMenuToggle: (
+        employee: Employee,
+        event: React.MouseEvent<HTMLButtonElement>,
+    ) => void;
 };
 
 export function EmployeeTable({ employees, onMenuToggle }: EmployeeTableProps) {
     return (
-        <table className="w-full caption-bottom text-sm">
-            <thead className="[&_tr]:border-b">
-                <tr>
-                    <th className="h-10 whitespace-nowrap px-2 text-left align-middle font-medium text-foreground">Emp ID</th>
-                    <th className="h-10 whitespace-nowrap px-2 text-left align-middle font-medium text-foreground">Name</th>
-                    <th className="h-10 whitespace-nowrap px-2 text-left align-middle font-medium text-foreground">Type</th>
-                    <th className="h-10 whitespace-nowrap px-2 text-left align-middle font-medium text-foreground">Rate</th>
-                    <th className="h-10 whitespace-nowrap px-2 text-left align-middle font-medium text-foreground">Date hired</th>
-                    <th className="h-10 whitespace-nowrap px-2 text-right align-middle font-medium text-foreground">Actions</th>
-                </tr>
-            </thead>
-            <tbody className="[&_tr:last-child]:border-0">
-                {employees.map((employee) => (
-                    <EmployeeTableRow
-                        key={employee.id}
-                        employee={employee}
-                        onMenuToggle={onMenuToggle}
-                    />
-                ))}
-            </tbody>
-        </table>
+        <div className="w-full overflow-x-auto overscroll-x-contain">
+            <table className="w-full min-w-[760px] caption-bottom text-sm">
+                <thead className="[&_tr]:border-b">
+                    <tr>
+                        <th className="h-10 whitespace-nowrap px-2 text-left align-middle font-medium text-foreground sm:px-4">
+                            Emp ID
+                        </th>
+                        <th className="h-10 whitespace-nowrap px-2 text-left align-middle font-medium text-foreground sm:px-4">
+                            Name
+                        </th>
+                        <th className="h-10 whitespace-nowrap px-2 text-left align-middle font-medium text-foreground sm:px-4">
+                            Type
+                        </th>
+                        <th className="h-10 whitespace-nowrap px-2 text-left align-middle font-medium text-foreground sm:px-4">
+                            Rate
+                        </th>
+                        <th className="h-10 whitespace-nowrap px-2 text-left align-middle font-medium text-foreground sm:px-4">
+                            Date hired
+                        </th>
+                        <th className="h-10 whitespace-nowrap px-2 text-right align-middle font-medium text-foreground sm:px-4">
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody className="[&_tr:last-child]:border-0">
+                    {employees.map((employee) => (
+                        <EmployeeTableRow
+                            key={employee.id}
+                            employee={employee}
+                            onMenuToggle={onMenuToggle}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
