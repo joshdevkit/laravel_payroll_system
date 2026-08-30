ad2935fb9138861b83092563d6b4913a6cd5f939
Joshua Pacho <102531192+joshdevkit@users.noreply.github.com>
2026-08-30 19:07:07 +0800

    Configure Inertia React TSX page resolution

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,10 +1,20 @@
 import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Payroll System';
 
 void createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.tsx`,
+            import.meta.glob('./pages/**/*.tsx'),
+        ),
     progress: {
         color: '#4B5563',
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
     },
 });
