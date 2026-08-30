094208f378409a7baec7ef8fce7ee2ad0abc8ef8
Joshua Pacho <102531192+joshdevkit@users.noreply.github.com>
2026-08-30 19:16:20 +0800

    Add Laravel employee CRUD routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthenticationController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthenticationController::class, 'login'])->name('login.submit');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return inertia('Dashboard/Index');
+    })->name('dashboard');
+
+    Route::get('/employees', [EmployeeController::class, 'index'])
+        ->name('employees.index');
+    Route::post('/employees', [EmployeeController::class, 'store'])
+        ->name('employees.store');
+    Route::put('/employees/{employee}', [EmployeeController::class, 'update'])
+        ->name('employees.update');
+    Route::delete('/employees/{employee}', [EmployeeController::class, 'destroy'])
+        ->name('employees.destroy');
+});
