5ac1a48c8af6d69efc16354f818f26d10090aa92
Joshua Pacho <102531192+joshdevkit@users.noreply.github.com>
2026-08-31 19:15:02 +0800

    Add PayrollRun model

---
+++ app/Models/PayrollRun.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
+
+class PayrollRun extends Model
+{
+    use HasFactory;
+
+    protected $table = 'payroll_runs';
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'cutoff_start',
+        'cutoff_end',
+        'pay_date',
+        'status',
+        'settings_snapshot',
+    ];
+
+    protected $casts = [
+        'cutoff_start' => 'date:Y-m-d',
+        'cutoff_end' => 'date:Y-m-d',
+        'pay_date' => 'date:Y-m-d',
+        'settings_snapshot' => 'array',
+    ];
+
+    protected static function booted(): void
+    {
+        static::creating(function (PayrollRun $run) {
+            if (! $run->getKey()) {
+                $run->setAttribute($run->getKeyName(), (string) Str::uuid());
+            }
+        });
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(PayrollItem::class);
+    }
+}
