03c1baa6f93069ba5b83498b7b5ed6e24a64b426
Joshua Pacho <jshpch1996@gmail.com>
2026-09-02 00:17:19 +0800

    redirect to / when logging out not on route post login

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -39,6 +39,6 @@ class AuthenticationController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect('/');
     }
 }
