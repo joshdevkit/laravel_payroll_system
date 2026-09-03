288846dd38b1428ab5ce94a9be3f46ca2a5949ac
Joshua Pacho <jshpch1996@gmail.com>
2026-09-03 18:34:29 +0800

    final fix v2

--- app/Http/Controllers/SchedulingController.php
+++ app/Http/Controllers/SchedulingController.php
@@ -53,7 +53,16 @@ class SchedulingController extends Controller
 
     public function update(Request $request, EmployeeSchedule $schedule): RedirectResponse
     {
-        $data = $this->validatedData($request);
+         $data = $request->validate([
+            'employee_id' => ['required', 'uuid', 'exists:employees,id'],
+            'work_date' => ['required', 'date'],
+            'start_time' => ['required_if:is_working_day,true', 'date_format:H:i'],
+            'end_time' => ['required_if:is_working_day,true', 'date_format:H:i'],
+            'break_minutes' => ['nullable', 'integer', 'min:0'],
+            'is_working_day' => ['required', 'boolean'],
+            'notes' => ['nullable', 'string', 'max:1000'],
+        ]);
+
         $data['employee_id'] = $schedule->employee_id;
         $data['segment_no'] = $schedule->segment_no;
         $data['break_minutes'] = $this->resolveBreakMinutes($data);
